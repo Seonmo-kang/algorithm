8cc7bfd2d56f7fd4751333d20e71bf62d787fab3
Seonmo Kang <tjsah18@gmail.com>
2022-01-18 17:32:10 -0500

    find Kth Largest element in int array (PriorityQueue)

--- leetcode/LargestElement.java
+++ leetcode/LargestElement.java
@@ -1,13 +1,20 @@
 package leetcode;
-
-import java.util.Arrays;
-import java.util.Collections;
-// import java.util.Collections;
+/*
+ *  find Kth Largest element in int array
+ *  using PriorityQueue 
+ */
+import java.util.PriorityQueue;
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Integer[] arr3 = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        // int 타입일 때는 Integer 타입 배열로 변경해 주어야 한다.
-        Arrays.sort(arr3, Collections.reverseOrder());
-        return arr3[k-1];
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            maxHeap.add(nums[i]);
+        }
+        
+        for(int j=0;j<k;j++){
+             result = maxHeap.poll();
+        }
+        return result;
     }
 }
